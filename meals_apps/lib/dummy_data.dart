1362e1d19d4757c61f4ae030e1ef7e930cb9a9a4
Md Anik Islam <anikislam.swe@gmail.com>
2021-05-18 10:05:04 +0600

    Meal Apps

---
+++ meals_apps/lib/dummy_data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'c1', title: "Italian", color: Colors.purple),
+  Category(id: 'c2', title: "Quick & Easy", color: Colors.red),
+  Category(id: 'c3', title: "Hamburgers", color: Colors.grey),
+  Category(id: "c4", title: "Pizza", color: Colors.blueGrey),
+  Category(id: "c5", title: "Pizza", color: Colors.brown),
+  Category(id: "c6", title: "Pizza", color: Colors.limeAccent),
+  Category(id: 'c1', title: "Italian", color: Colors.purple),
+  Category(id: 'c2', title: "Quick & Easy", color: Colors.red),
+  Category(id: 'c3', title: "Hamburgers", color: Colors.grey),
+  Category(id: "c4", title: "Pizza", color: Colors.blueGrey),
+  Category(id: "c5", title: "Pizza", color: Colors.brown),
+  Category(id: "c6", title: "Pizza", color: Colors.limeAccent),
+  Category(id: "c6", title: "Pizza", color: Colors.limeAccent),
+  Category(id: 'c1', title: "Italian", color: Colors.purple),
+  Category(id: 'c2', title: "Quick & Easy", color: Colors.red),
+  Category(id: 'c3', title: "Hamburgers", color: Colors.grey),
+  Category(id: "c4", title: "Pizza", color: Colors.blueGrey),
+  Category(id: "c5", title: "Pizza", color: Colors.brown),
+
+];
